4f8e94850da7563853a17f7a8ef04830ff5a978a
gxnnie <lloydmanoto@icloud.com>
2022-10-15 05:56:57 -0700

    my first line of C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - print a line of ode usong puts
+ * main -print a line of ode usong puts
  *
  * Return: 0
-*
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
-		
+}		
