c441e7855ff1e520bbaee1b27b07d5c66b82e2eb
gxnnie <lloydmanoto@icloud.com>
2022-10-18 05:32:50 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * main -prints all possible combinations of single-digit numbers
+ *
+ * Return: 0
+**/
+int main(void)
+{
+	int b;
+
+	for  (b = 48; b < 58; b++)
+	{
+
+		putchar(b);
+		if (b != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
