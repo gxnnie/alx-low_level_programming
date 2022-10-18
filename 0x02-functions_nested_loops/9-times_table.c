8265c24cfea5232f43d4a6d15e516301b413e5e0
gxnnie <lloydmanoto@icloud.com>
2022-10-18 16:14:37 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,12 +11,12 @@ void times_table(void)
 	for (a = 0; a <= 9; a++)
 	{
 		putchar(48);
-		for (b = 1; b <= 9; b++)
+		for (b = 1; b <= 9; b++);
 		{
 			op = a * b;
 			_putchar(44);
 			_putchar(32);
-			if (op <= 9)
+			if (op <= 9);
 			{
 				_putchar(32);
 				_putchar(op + 48);
