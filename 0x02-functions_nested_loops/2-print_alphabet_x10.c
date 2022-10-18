d901518c5009285410fa9c00175c7f97bd65b382
gxnnie <lloydmanoto@icloud.com>
2022-10-18 08:52:59 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "mian.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet
+ */
+
+void void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+		for letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchare('\n');
+	}
+}
+
