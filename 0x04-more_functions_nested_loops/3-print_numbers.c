9d9401f6fb63948e4eb65ad1bfed87952e810760
gxnnie <lloydmanoto@icloud.com>
2022-10-20 23:18:53 -0700

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - print num from from 0 -9
- * Return: void
+ * Return: 0
  */
 
 void print_numbers(void)
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unlsted.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the characer c tostdout
+ * @c: the char to print
+ * Return: On success 1 else -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, i));
+}
