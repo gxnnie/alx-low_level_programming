1cfa88df7b3d9db18140669ede6463e00a414f36
gxnnie <lloydmanoto@icloud.com>
2022-10-21 11:00:50 -0700

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,16 +9,26 @@ int main(void)
 {
 	int n;
 
-	printf("1");
+	n = 1;
+	printf("%d" n);
 	for (n = 2; n <= 100; n++)
 	{
-		printf(" ");
-		if (m % 3-- 0)
-			printf("fizz");
-		if (n % 5 == 0)
-			printf("buzz");
-		if (n % 3 != 0 && n % 5 != 0)
-			printf("%d", n);
+		if((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d",n);
+		}
 	}
 	printf("\n");
 	return (0);
