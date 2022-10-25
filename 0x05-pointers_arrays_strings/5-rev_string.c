8972127944dbc364d54080433ee4e6491c696cc8
gxnnie <lloydmanoto@icloud.com>
2022-10-25 04:32:29 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,24 +6,17 @@
  */
 void rev_string(char *s)
 {
-	int count = 0,i,j;
-	char *str, temp;
+	char rv = s[0];
+	int c = 0;
+	int i;
 
-	while (count >= 0)
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
 	{
-		if (s[count] =='\0')
-			break;
-		count++;
-	}
-	str = s;
-
-	for (i = 0; i < (count - 1); i++)
-	{
-		for (j = i + 1; j > 0; j--)
-		{
-			temp = *(str + j);
-			(str + j) = (str + (j - 1));
-			*(str + (j - 1) = temp);
-		}
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
 	}
 }
